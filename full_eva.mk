75b0a448f7e34246bd9afa541841205ac1592810
Paul O'Brien <paul@modaco.com>
2016-05-25 11:31:50 +0100

    Fixed incorrect device name in full_ file

--- full_eva.mk
+++ full_eva.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/huawei/kiwi
+LOCAL_PATH := device/huawei/eva
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
